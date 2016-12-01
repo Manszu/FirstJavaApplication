ba1aa2c0b25d1298100282551a7a45faa41284b9
Krzysztof Mansz <krzysztof.mansz@gmail.com>
2016-12-01 18:12:41 +0100

    Initial commit

---
+++ src/com/javathlon/lecture1/FirstClass.java
@@ -0,0 +1,22 @@
+package com.javathlon.lecture1; //package statement
+
+/**
+ * Created by Comarch on 2016-11-30.
+ */
+public class FirstClass {
+
+    public static void main(String args[]){
+
+        int count ;
+        count = 0;
+
+        System.out.println(count);
+        System.out.println("Hello world");
+
+        count = 1;
+
+        System.out.println(count);
+
+    }
+
+}
